655f9edb680b11459cdcecbba6e3bec1a7c6ae0a
Sarav <me@sarav.co>
2017-10-29 22:53:15 +0530

    Removed API dead code

--- src/code/Auth/routes/api.php
+++ src/code/Auth/routes/api.php
@@ -1,8 +1,1 @@
 <?php
-
-Route::group(["prefix" => "v1"], function(){
-    Route::post('register', 'Api\AuthController@register');
-    Route::post('otp/generate', 'Api\AuthController@generateOtp');
-    Route::post('login', 'Api\AuthController@login');
-    Route::post('id/forgot', 'Api\AuthController@mailLoginId');
-});
